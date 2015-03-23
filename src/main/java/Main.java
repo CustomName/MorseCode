842381f69e17198e40d8401afe51a4470a08e76a
Alexey R <radionenko.alexey@gmail.com>
2015-03-23 19:05:42 +0400

    Точка входа в приложение

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Алексей
+ * 3/23/2015
+ */
+public class Main {
+
+    /**
+     * Точка входа в приложение
+     * @param args
+     */
+    public static void main(String[] args){
+
+        System.out.println("MorseCode: hello!");
+
+    }
+
+}
